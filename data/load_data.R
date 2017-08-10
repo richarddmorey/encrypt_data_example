f2d9676bfe211001150914881671623e92480299
richarddmorey <richarddmorey@gmail.com>
2017-08-10 22:41:33 +0100

    clarity, read nonce from GitHub

--- data/load_data.R
+++ data/load_data.R
@@ -1,7 +1,6 @@
 require(sodium)
 # See https://cran.r-project.org/web/packages/sodium/vignettes/intro.html
 
-
 ## When you're ready to share, just release the correct passphrase
 ## in the load_data.R script. Alternatively, you could load 
 ## this from a local file that you later share others.
@@ -10,12 +9,12 @@ key_string = "This is a secret passphrase"
 ## Hash the key (as when encrypted)
 key = hash(charToRaw(key_string))
 
-## Read in the nonce
-nonce_char = scan("nonce", what = "character")
+## Read in the nonce from GitHub
+nonce_char = scan("https://github.com/richarddmorey/encrypt_data_example/blob/master/data/nonce?raw=true", what = "character")
 nonce = hex2bin(paste(nonce_char, collapse = ""))
 
-## Read from GitHub
-gh_con = url("https://github.com/richarddmorey/encrypt_data_example/blob/master/exp1_encrypted.rds?raw=true")
+## Read encrypted data from GitHub
+gh_con = url("https://github.com/richarddmorey/encrypt_data_example/blob/master/data/exp1_encrypted.rds?raw=true")
 cipher = readRDS(gzcon(gh_con))
 close(gh_con)
 
