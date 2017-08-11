fcaf85d4af18753773f450aba940b5b5b87f946e
richarddmorey <richarddmorey@gmail.com>
2017-08-11 01:39:33 +0100

    added example code to load from OSF

--- data/load_data.R
+++ data/load_data.R
@@ -11,12 +11,19 @@ key = hash(charToRaw(key_string))
 
 ## Read in the nonce from GitHub
 nonce_char = scan("https://github.com/richarddmorey/encrypt_data_example/blob/master/data/nonce?raw=true", what = "character")
+## Read in the nonce from OSF
+# nonce_char = scan("https://osf.io/n6k4z/download?version=1", what = "character")
 nonce = hex2bin(paste(nonce_char, collapse = ""))
 
 ## Read encrypted data from GitHub
 gh_con = url("https://github.com/richarddmorey/encrypt_data_example/blob/master/data/exp1_encrypted.rds?raw=true")
 cipher = readRDS(gzcon(gh_con))
 close(gh_con)
+## Read encrypted data from OSF
+# osf_con = url("https://osf.io/2k84f/download?version=1")
+# cipher = readRDS(gzcon(osf_con))
+# close(osf_con)
+
 
 ## Decrypt and unserialize
 s_dat = data_decrypt(cipher, key, nonce)
